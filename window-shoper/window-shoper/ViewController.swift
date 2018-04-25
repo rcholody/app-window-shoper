e126f63a84424fe472ba1d32fc39c38aa494af66
Rafał Chołody <Rafal@MacBook-Pro-MacBook.local>
2018-04-25 10:30:36 +0200

    first commit

---
+++ window-shoper/window-shoper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  window-shoper
+//
+//  Created by Rafał Chołody on 25/04/2018.
+//  Copyright © 2018 Rafał Chołody. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
